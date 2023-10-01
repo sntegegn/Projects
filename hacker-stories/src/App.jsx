4405dc3cfaa8a8df6e03996614558392ff6bd9bb
Sntegegn <selamnigussie41@gmail.com>
2023-10-01 09:21:49 -0400

    Learn React

---
+++ hacker-stories/src/App.jsx
@@ -0,0 +1,82 @@
+import * as React from 'react'
+/* eslint-disable react/prop-types */
+
+const App = () =>  {
+  const stories = [
+    {
+      title : 'React',
+      url : 'https://reactjs.org/',
+      author : 'Jordan Walke',
+      num_comments : 3,
+      points : 4,
+      objectID: 0,
+    },
+    {
+      title : 'Redux',
+      url : 'https://redux.js.org/',
+      author : 'Dan Abramov, Andrew Clark',
+      num_comments : 2,
+      points : 5,
+      objectID: 1,
+    }
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('React');
+
+  const handleSearch = (event) =>{
+    setSearchTerm(event.target.value)
+  };
+
+  const searchedStories = stories.filter((story) => 
+      story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+  return(
+    <div>
+      <h1>
+        Hello World
+      </h1>
+
+      <Search search = {searchTerm} onSearch = {handleSearch}/>
+      <p>
+        This is what you searched {searchTerm}
+      </p>
+      <hr />
+      <List list= {searchedStories} />
+
+    </div>
+  )}
+
+const Search = ({search, onSearch}) => {
+  return(
+    <div>
+      <label htmlFor='search'>Search</label>
+      <input id = 'search' type='text' value = {search} onChange= {onSearch}/>
+
+    </div>
+    )
+}
+
+const List = ( {list} ) => (
+    <ul>
+    {list.map(( {objectID, ...item} ) => (
+      <Item key = {objectID} {...item} />
+    ))}
+    </ul>
+  );
+
+  const Item = ({title, url, author, num_comments, points}) => {
+    return(   
+      <li>
+        <span>
+          <a href = {url}>{title}</a>
+        </span>
+        <span>{author}</span>
+        <span>{num_comments}</span>
+        <span>{points}</span>
+      </li>
+    )
+  }
+
+
+
+export default App
